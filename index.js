46f2c113bc1a88f0cfb75ebc187e956df59164cc
Biplob Hossain <biplob17h@gmail.com>
2024-01-18 03:05:19 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
+
+// APP
+const app = express();
+
+// CONFIG
+dotenv.config();
+
+//MIDDLEWIRES
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// ROUTES
+
+// HOMEPAGE
+app.get("/", (req, res) => {
+  res.send(`<h1>Wellcome to blog page server</h1>`);
+});
+
+// LISTEN
+const port = process.env.PORT || 8080;
+connectDB()
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`.green.bgWhite.bold);
+});
